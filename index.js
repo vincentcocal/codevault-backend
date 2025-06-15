b777cdb2bd98c9aa64b78eb0a2f076fe46d6644a
vincentcocal <vincentcocal0314@gmail.com>
2025-06-15 13:27:22 +0800

    additional on index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://codevault-frontend-b511.vercel.app/'
+    'https://codevault-frontend-b511.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
